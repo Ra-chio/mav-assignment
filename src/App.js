75c0fc7891df7a8b0ccaf82c7d79c9d025be1357
Rachana S <109232556+Ra-chio@users.noreply.github.com>
2022-12-14 23:04:27 +0530

    added protected route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles.css";
 import CreateUser from "./routes/CreateUser";
 import EmpDetails from "./routes/EmpDetails";
 import EmpDirectory from "./routes/EmpDirectory";
 import Login from "./routes/Login";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
     return (
-        <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/create-user" element={<CreateUser />} />
-            <Route
-                exact
-                path="/employee-directory"
-                element={<EmpDirectory />}
-            />
-            <Route
-                exact
-                path="/employee-details/:id"
-                element={<EmpDetails />}
-            />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route exact path="/create-user" element={<CreateUser />} />
+                {/* <Route
+                    path="/employee-directory"
+                    element={<ProtectedRoute component={EmpDirectory} />}
+                />
+                <Route
+                    exact
+                    path="/employee-details/:id"
+                    element={<EmpDetails />}
+                /> */}
+                <Route element={<ProtectedRoute />}>
+                    <Route
+                        path="/employee-directory"
+                        element={<EmpDirectory />}
+                    />
+                    <Route
+                        exact
+                        path="/employee-details/:id"
+                        element={<EmpDetails />}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
